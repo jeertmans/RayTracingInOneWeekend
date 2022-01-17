66772b412286f67ba3218b6c99f20055859aa5e8
Jérome Eertmans <jeertmans@icloud.com>
2022-01-17 17:16:42 +0100

    Up to section 6.2

--- rtionewe/cli.py
+++ rtionewe/cli.py
@@ -1,14 +1,18 @@
 import click
-from .images import Image
-from .scenes import example_scene
-from .vectors import ray_color, vector
-import numpy as np
 
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.option("-W", "--width", default=256, type=int, help="Image width.")
 @click.option("-H", "--height", default=256, type=int, help="Image height.")
-def cli(width, height):
+@click.pass_context
+def cli(ctx, width, height):
+    if ctx.invoked_subcommand is not None:
+        return
+
+    from .images import Image
+    from .scenes import example_scene
+    from .vectors import ray_color, vector
+    import numpy as np
 
     array = example_scene(width=width, height=height)
     image = Image.from_scene_array(array)
@@ -16,20 +20,15 @@ def cli(width, height):
     image.save("test.png")
 
 
-@click.group()
-def cache():
-    pass
-
-
-@cache.command()
-def clear():
+@cli.command()
+def clear_cache():
     import os
     import shutil
+
     dirname = os.path.dirname(__file__)
     shutil.rmtree(os.path.join(dirname, "__pycache__"))
+    click.secho("Cache folder was cleared!", fg="green")
 
-
-cli.add_command(cache)
 
 if __name__ == "__main__":
     cli()
